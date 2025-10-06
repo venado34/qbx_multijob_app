5daab9dc5f411a24ee875aed486f2928991f1ee9
danielj <danieljlcso@gmail.com>
2025-10-05 23:56:02 -0600

    update depend

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,26 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
-author 'Venado'
-title 'LB Phone - Job Selector'
-description 'An app to select your current job.'
-version '1.0.0'
+author "Venado"
+title "LB Phone - Job Selector"
+description "An app to select your current job."
 
-shared_script 'config.lua'
-
-client_scripts {
-    '@qbx_core/modules/playerdata.lua',
-    'client/main.lua'
+dependencies {
+    'qbx_core',
+    'ox_lib',
+    'lb-phone'
 }
 
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_script 'client/main.lua'
 server_script 'server.lua'
 
 ui_page "ui/dist/index.html"
 
 files {
     "ui/dist/**/*"
-}
-
-dependencies {
-    'qbx_core',
-    'lb-phone',
-    'ox_lib'
 }
