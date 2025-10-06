9fa8bc687223444ccf585105f183779537a7fc44
danielj <danieljlcso@gmail.com>
2025-10-06 00:59:43 -0600

    corrected luas

--- client/main.lua
+++ client/main.lua
@@ -1,12 +1,10 @@
 if Config.Debug then print("[JobSelector] Client script started.") end
 
--- Wait for lb-phone to be ready before trying to add the app.
 while GetResourceState("lb-phone") ~= "started" do
     Wait(500)
 end
 if Config.Debug then print("[JobSelector] lb-phone has started.") end
 
--- Function to register the app with lb-phone.
 local function AddApp()
     exports["lb-phone"]:AddCustomApp({
         identifier = Config.Identifier,
@@ -22,23 +20,30 @@ local function AddApp()
 end
 AddApp()
 
--- This is triggered by the UI when the app is opened.
+-- This function is called by the UI when it needs the job list.
 RegisterNUICallback('getJobs', function(data, cb)
-    -- It uses the standard QBCore callback system to ask the server for the list of available jobs.
-    QBCore.Functions.TriggerCallback('job-selector:server:getAvailableJobs', function(availableJobs)
-        -- Once the server responds, it sends the list back to the UI.
+    -- Create a temporary event handler to wait for the server's response.
+    local function handleJobsResponse(availableJobs)
+        if Config.Debug then print("[JobSelector] Client: Received jobs from server: " .. json.encode(availableJobs)) end
         cb(availableJobs)
-    end)
+        -- Clean up the event handler after we get the response to prevent memory leaks.
+        RemoveEventHandler('job-selector:client:receiveAvailableJobs', handleJobsResponse)
+    end
+
+    -- Register the handler and then trigger the server event to ask for the jobs.
+    AddEventHandler('job-selector:client:receiveAvailableJobs', handleJobsResponse)
+    if Config.Debug then print("[JobSelector] Client: Requesting available jobs from server.") end
+    TriggerServerEvent('job-selector:server:requestAvailableJobs')
 end)
 
--- This is triggered by the UI when a job button is clicked.
+-- This function is called by the UI when a job button is clicked.
 RegisterNUICallback('setJob', function(data, cb)
     if data and data.jobName then
-        -- It tells the server to run the 'changeJob' event.
+        if Config.Debug then print("[JobSelector] Client: Sending job change event to server for job: " .. data.jobName) end
         TriggerServerEvent('job-selector:server:changeJob', data.jobName)
         cb('ok')
     else
+        if Config.Debug then print("[JobSelector] Client ERROR: 'setJob' callback received invalid data.") end
         cb('error')
     end
 end)
-
--- server.lua
+++ server.lua
@@ -1,28 +1,27 @@
-if Config.Debug then print("[JobSelector] Server script has started and is waiting for QBCore to be ready.") end
+if Config.Debug then print("[JobSelector] Server script started.") end
 
--- This event handler ensures that the code inside only runs after QBCore is fully loaded.
-AddEventHandler('QBCore:Ready', function()
-    if Config.Debug then print("[JobSelector] ### QBCore IS READY! ### Registering server callbacks.") end
+-- This is a standard event that the client will trigger directly.
+RegisterNetEvent('job-selector:server:requestAvailableJobs', function()
+    local source = source
+    local Player = exports.qbx_core:GetPlayer(source)
 
-    -- This is the standard QBCore way to create a callback that the client can request data from.
-    QBCore.Functions.CreateCallback('job-selector:server:getAvailableJobs', function(source, cb)
-        local Player = exports.qbx_core:GetPlayer(source)
-        if not Player then
-            cb({})
-            return
-        end
+    if not Player then
+        if Config.Debug then print("[JobSelector] Server ERROR: Could not get player object for source " .. tostring(source)) end
+        return
+    end
 
-        local availableJobs = {}
-        if Player.PlayerData.jobs and next(Player.PlayerData.jobs) then
-            for jobName, grade in pairs(Player.PlayerData.jobs) do
-                if jobName ~= Player.PlayerData.job.name then
-                    availableJobs[jobName] = grade
-                end
+    local availableJobs = {}
+    if Player.PlayerData.jobs and next(Player.PlayerData.jobs) then
+        for jobName, grade in pairs(Player.PlayerData.jobs) do
+            if jobName ~= Player.PlayerData.job.name then
+                availableJobs[jobName] = grade
             end
         end
-        
-        cb(availableJobs)
-    end)
+    end
+    
+    if Config.Debug then print("[JobSelector] Server: Sending available jobs to client: " .. json.encode(availableJobs)) end
+    -- We send the data back to the client with a separate event.
+    TriggerClientEvent('job-selector:client:receiveAvailableJobs', source, availableJobs)
 end)
 
 -- This event is triggered by the client when a job button is pressed.
@@ -32,5 +31,6 @@ RegisterNetEvent('job-selector:server:changeJob', function(jobName)
 
     if not Player then return end
 
+    if Config.Debug then print(string.format("[JobSelector] Server: Changing primary job for %s to %s", Player.PlayerData.citizenid, jobName)) end
     exports.qbx_core:SetPlayerPrimaryJob(Player.PlayerData.citizenid, jobName)
 end)
